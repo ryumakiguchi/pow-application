69bc20a5fb63a2c0b2a49d9c5b0c9a9137f76671
Ryusei Makiguchi <makiguchir@gmail.com>
2022-10-14 16:32:06 -0300

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,3 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-
-Golfer.destroy_all
-
-Golfer.create(name: 'Juan', handicap: 17.3, review: "Plays slow")
-Golfer.create(name: 'Facundo', handicap: 3.2, review: "Great to play with")
